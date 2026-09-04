5fe816aa233ca369718ce5077bf8bb57b7712d5d
Piyush-Geetesh <piyush.geetesh2000@gmail.com>
2026-09-05 04:02:22 +0530

    Configure Django allowed hosts for Vercel

--- config/settings.py
+++ config/settings.py
@@ -23,12 +23,19 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/5.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = "django-insecure-r@92297(m=womcsy+gd9&b-a40cei38$#6h!hz*z@+hq18-7h)"
+SECRET_KEY = os.environ.get(
+    "DJANGO_SECRET_KEY",
+    "django-insecure-local-development-key"
+)
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = os.environ.get("DEBUG", "True").lower() == "true"
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = [
+    "127.0.0.1",
+    "localhost",
+    ".vercel.app",
+]
 
 
 # Application definition
